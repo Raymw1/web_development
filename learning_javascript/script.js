6a3d7f6e5e353fed47bc3a8357ec734c8df57c20
Rayan Wilbert <rayan.rmw@gmail.com>
2021-04-21 18:43:53 -0300

    Starting to learn JavaScript

---
+++ learning_javascript/script.js
@@ -0,0 +1,24 @@
+/*
+Mult-line comments
+*/
+
+console.log("Welcome to Starter!")  // Print in JavaScript
+
+// It's an object(dict)
+let myObject = {
+    name: "Rayan",
+    age: 17, 
+    genre: "Male",
+    walk: function() {
+        console.log("Walking...")
+    }
+}
+
+// It's an array
+let myArray = [
+    "Milk",
+    "Eggs",
+    "Donuts",
+    2,
+    3
+]
